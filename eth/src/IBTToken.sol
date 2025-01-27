a1647abe7a442b6a4488e6c86a9289bf3d001103
andreisirbu-ibt <andrei.sirbu03@e-uvt.ro>
2025-01-27 13:16:45 +0200

    Add files via upload

---
+++ eth/src/IBTToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract IBTToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("IBTToken", "IBT") Ownable(initialOwner) {
+        // Additional initialization logic if needed
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
